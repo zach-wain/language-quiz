f156416f00617cd94d3eb9eb412da6ed3b78a6c5
Zach <wainz@middleton.school.nz>
2020-09-05 22:20:08 +1200

    Introduced judge_score function to give player some feedback

--- 04_summary-v2.py
+++ 04_summary-v2.py
@@ -9,6 +9,16 @@ number_of_questions = 10
 point_value = 10
 keep_playing = True
 
+def judge_score():
+    if score_counter >= point_value * number_of_questions:
+        return "Excellent, you know your stuff!"
+    elif score_counter >=70:
+        return "Nice work, you're getting there!"
+    elif score_counter >=30:
+        return "You need some more practice"
+    else:
+        return "Are you even trying?"
+
 def ask_for_decision(question, positive_answer):
     decision = input(question)
     return decision.lower().startswith(positive_answer)
@@ -19,6 +29,7 @@ while keep_playing:
 
     # game finished
     print("Your total score was: {} out of {}".format(score_counter, number_of_questions * point_value))
+    print(judge_score())
     if ask_for_decision("Would you like to play again (Y/N)? ", "n"):
         if ask_for_decision("Are you sure you want to quit (Y/N)?", "y"):
             keep_playing = False
